c1dc13592f918014c5c8ffc14dfe0a81f811aced
seankim658 <33474168+seankim658@users.noreply.github.com>
2024-03-05 16:01:56 -0500

    add .gitignore

--- app/wrap.sh
+++
@@ -1,4 +0,0 @@
-sudo systemctl stop docker-hivelab-app-tst.service
-python3 create_app_container.py -s tst
-sudo systemctl start docker-hivelab-app-tst.service
-
--- sync.sh
+++
@@ -1,4 +0,0 @@
-git add -A
-git commit -m "Towards portable APP"
-git pull 
-git push
